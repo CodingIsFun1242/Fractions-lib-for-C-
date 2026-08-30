2a2c5cf5e617075697e4877fd893e330dd06a884
CodingIsFun1242 <jackyvelarde@outlook.com>
2026-08-30 16:44:58 +0800

    The script

---
+++ fractions.h
@@ -0,0 +1,431 @@
+/*fractions.h */
+/*
+ * Copyright (C) 2026 王家祺(Jacky Velarde Z.) <3594929067@qq.com>
+ *
+ * This file is released under the GPLv2 (or later, at your option).
+ */
+
+#ifndef FRACTIONS_H
+#define FRACTIONS_H
+
+
+#include <cstdlib>
+#include<iostream>
+#include <cmath>
+#include <string>
+#include <stdexcept>
+
+template <typename T>
+static inline T gcd(T a, T b)
+{
+	a = std::abs(a);
+	b = std::abs(b);
+	while (b != 0)
+	{
+		T temp = b;
+		b = a % b;
+		a = temp;
+
+	}
+	return a;
+}
+inline static int Decimal_count(double in) {
+	std::string s = std::to_string(in);
+	const size_t pos = s.find('.');
+	if (pos == std::string::npos) return 0;
+	const size_t end = s.find_last_not_of('0');
+	if (end == std::string::npos || s[end] == '.') return 0;
+	return static_cast<int>(end - pos);
+}
+namespace mfc{ //mfc, aka.Math for C++.
+	class fraction {
+	private:
+		int num = 0;//numerator
+		int den = 1;//denominator
+	public:
+		fraction(int n, int d) : num(n), den(d) { 
+			if (d == 0) {
+				throw std::invalid_argument("Denominator cannot be zero");
+			}
+			if (den < 0) {
+				num = -num;
+				den = -den;
+			}
+		}
+		fraction() : num(0), den(1) {}
+		fraction(int n) : num(n), den(1) {}
+		fraction(double n) : num(0), den(1)
+		{
+			fraction temp1(n * std::pow(10, Decimal_count(n)), std::pow(10, Decimal_count(n)));
+			temp1 = temp1.simplify();
+			num = temp1.num;
+			den = temp1.den;
+		}
+		double solve() const; 
+		float sm_solve() const;
+		long double large_solve() const;
+		fraction pow_int(int a) const;
+		fraction rec() const;//Basically makes the fraction upside down.
+		fraction& abs(); //This modifys the fraction.
+		fraction rtrn_abs() const; //This returns the result instead of modifying the fraction itself.
+		fraction simplify() const
+		{
+			int g = gcd(std::abs(num), std::abs(den));
+			return fraction(num / g, den / g);
+		}
+		void swap(fraction& other) noexcept
+		{
+			std::swap(num, other.num);
+			std::swap(den, other.den);
+		}
+		fraction& operator = (fraction other) noexcept
+		{
+			swap(other);
+			return *this;
+		}
+		fraction operator - () const noexcept
+		{
+			fraction Temp(-num, den);
+			return Temp;
+		}
+		fraction operator + (const fraction& other) const noexcept
+		{
+			int lcm = den / gcd(den, other.den) * other.den;
+			int new_num = num * (lcm / den) + other.num * (lcm / other.den);
+			fraction result(new_num, lcm);
+			return result.simplify();
+		}
+		fraction operator + (const int& other) const noexcept
+		{
+			fraction result(num + other * den, den);
+			return result.simplify();
+		}
+		fraction operator + (const double& other) const noexcept
+		{
+			fraction temp(other);
+			fraction temp2(this->num * temp.den + temp.num * this->den, this->den * temp.den);
+			return temp2.simplify();
+		}
+		fraction operator - (const fraction& other) const noexcept
+		{
+			int lcm = den / gcd(den, other.den) * other.den;
+			int new_num = num * (lcm / den) - other.num * (lcm / other.den);
+			fraction result(new_num, lcm);
+			return result.simplify();
+		}
+		fraction operator - (const int& other) const noexcept
+		{
+			fraction result(num - other * den, den);
+			return result.simplify();
+		}
+		fraction operator - (const double& other) const noexcept
+		{
+			fraction temp(other);
+			fraction temp2(this->num * temp.den - temp.num * this->den,this->den * temp.den);
+			return temp2.simplify();
+		}
+		fraction operator * (const fraction& other) const 
+		{
+			fraction result(num * other.num, den * other.den);
+			return result.simplify();
+		}
+		fraction operator * (const int& other) const 
+		{
+			fraction result(num * other, den);
+			return result.simplify();
+		}
+		fraction operator * (const double& other) const 
+		{
+			fraction temp(other);
+			fraction temp2(this->num * temp.num, this->den * temp.den);
+			return temp2.simplify();
+		}
+		fraction operator / (const fraction& other) const 
+		{
+			fraction result(num * other.den, den * other.num);
+			return result.simplify();
+		}
+		fraction operator / (const int& other) const 
+		{
+			if (other == 0)
+			{
+				throw std::invalid_argument("It ain't a smart choice to use 0 as a divisor");
+			}
+			else {
+				fraction result(num, den * other);
+				return result.simplify();
+			}
+		}
+		fraction operator / (const double& other) const noexcept
+		{
+			fraction temp(other);
+			fraction temp2(this->num * temp.den, this->den * temp.num);
+			return temp2.simplify();
+		}
+		fraction& operator+=(const fraction& other) noexcept {
+			*this = *this + other;
+			return *this;
+		}
+		fraction& operator-=(const fraction& other) noexcept {
+			*this = *this - other;
+			return *this;
+		}
+		fraction& operator*=(const fraction& other) noexcept {
+			*this = *this * other;
+			return *this;
+		}
+		fraction& operator/=(const fraction& other) noexcept {
+			*this = *this / other;
+			return *this;
+		}
+		fraction& operator++() noexcept { num += den; return *this; }
+		fraction& operator--() noexcept { num -= den; return *this; }
+		fraction operator++(int) noexcept {
+			fraction temp = *this;
+			num += den;
+			return temp;
+		}
+		fraction operator--(int) noexcept {
+			fraction temp = *this;
+			num -= den;
+			return temp;
+		}
+	
+		friend std::ostream& operator<<(std::ostream& os, const fraction& a) noexcept
+		{
+			os << a.num << "/" << a.den;
+			return os;
+		}
+		friend std::istream& operator>>(std::istream& is, fraction& a) noexcept
+		{
+			std::cout << "Num.:";
+			is >> a.num;
+			std::cout << "Den.:";
+			is >> a.den;
+			return is;
+		}
+		bool operator < (const fraction& other) const noexcept
+		{
+			if (this->num / this->den != other.num / other.den)
+			{
+				return this->num / this->den < other.num / other.den;
+			}
+			else {
+				return (this->num % this->den) * other.den < (other.num % other.den) * this->den;
+			}
+		}
+		bool operator <= (const fraction& other) const noexcept
+		{
+			if (this->num / this->den != other.num / other.den)
+			{
+				return this->num / this->den <= other.num / other.den;
+			}
+			else {
+				return (this->num % this->den) * other.den <= (other.num % other.den) * this->den;
+			}
+		}
+		bool operator != (const fraction& other) const noexcept
+		{
+			if (this->num / this->den != other.num / other.den)
+			{
+				return true;
+			}
+			else {
+				return (this->num % this->den) * other.den != (other.num % other.den) * this->den;
+			}
+		}
+		bool operator > (const fraction& other) const noexcept
+		{
+			if (this->num / this->den != other.num / other.den)
+			{
+				return this->num / this->den > other.num / other.den;
+			}
+			else {
+				return (this->num % this->den) * other.den > (other.num % other.den) * this->den;
+			}
+		}
+		bool operator >= (const fraction& other) const noexcept
+		{
+			if (this->num / this->den != other.num / other.den)
+			{
+				return this->num / this->den >= other.num / other.den;
+			}
+			else {
+				return (this->num % this->den) * other.den >= (other.num % other.den) * this->den;
+			}
+		}
+		bool operator < (const int& other) const noexcept
+		{
+			return this->sm_solve() < other;
+		}
+		bool operator != (const int& other) const noexcept
+		{
+			return this->sm_solve() != other;
+		}
+		bool operator <= (const int& other) const noexcept
+		{
+			return this->sm_solve() <= other;
+		}
+		bool operator > (const int& other) const noexcept
+		{
+			return this->sm_solve() > other;
+		}
+		bool operator >= (const int& other) const noexcept
+		{
+			return this->sm_solve() >= other;
+		}
+		bool operator < (const double& other) const noexcept
+		{
+			if (this->num / this->den != static_cast<int>(other))
+			{
+				return this->num / this->den < static_cast<int>(other);
+			}
+			else {
+				return this->num % this->den < (other - static_cast<int>(other)) * this->den;
+			}
+		}
+		bool operator != (const double& other) const noexcept
+		{
+			if (this->num / this->den != static_cast<int>(other))
+			{
+				return true;
+			}
+			else {
+				return this->num % this->den != (other - static_cast<int>(other)) * this->den;
+			}
+		}
+		bool operator <= (const double& other) const noexcept
+		{
+			if (this->num / this->den != static_cast<int>(other))
+			{
+				return this->num / this->den <= static_cast<int>(other);
+			}
+			else {
+				return this->num % this->den <= (other - static_cast<int>(other)) * this->den;
+			}
+		}
+		bool operator > (const double& other) const noexcept
+		{
+			if (this->num / this->den != static_cast<int>(other))
+			{
+				return this->num / this->den > static_cast<int>(other);
+			}
+			else {
+				return this->num % this->den > (other - static_cast<int>(other)) * this->den;
+			};
+		}
+		bool operator >= (const double& other) const noexcept
+		{
+			if (this->num / this->den != static_cast<int>(other))
+			{
+				return this->num / this->den >= static_cast<int>(other);
+			}
+			else {
+				return this->num % this->den >= (other - static_cast<int>(other)) * this->den;
+			};
+		}
+		bool operator == (const fraction& other) const noexcept
+		{
+			if (this->num / this->den != other.num / other.den)
+			{
+				return this->num / this->den == other.num / other.den;
+			}
+			else {
+				return (this->num % this->den) * other.den == (other.num % other.den) * this->den;
+			}
+		}
+		bool operator == (const int& other) const noexcept
+		{
+			return this->sm_solve() == other;
+		}
+		bool operator == (const double& other) const noexcept
+		{
+			if (this->num / this->den != static_cast<int>(other))
+			{
+				return this->num / this->den == static_cast<int>(other);
+			}
+			else {
+				return this->num % this->den == (other - static_cast<int>(other)) * this->den;
+			};
+		}
+	};
+	double fraction::solve() const//Returns double
+	{
+		return static_cast<double>(num) / den;
+	}
+	float fraction::sm_solve() const//Returns float,sm--small
+	{
+		return static_cast<float>(num) / den;
+	}
+	long double fraction::large_solve() const//Returns long double(Might not often be used)
+	{
+		return static_cast<long double>(num) / den;
+	}
+	bool compare(fraction one, fraction two)//This fraction is useless, but I still kept it. Don't ask why.
+	{
+		return one.sm_solve() > two.sm_solve();
+	}
+	fraction fraction::rec() const
+	{
+		fraction temp(den, num);
+		return temp.simplify();
+	}
+	fraction fraction::pow_int(int a) const
+	{
+		if (a > 0)
+		{
+			fraction temp(static_cast<int>(std::pow(num, a)), static_cast<int>(std::pow(den, a)));
+			return temp.simplify();
+		}
+		if (a == 0)
+		{
+			fraction b(1, 1);
+			return b;
+		}
+		else
+		{
+			fraction Ntemp = this->rec();
+			return Ntemp.pow_int(-a);
+		}
+	}
+	fraction& fraction::abs()
+	{
+		if (num < 0)
+		{
+			this->num = -num;
+		}
+		return *this;
+	}
+	fraction fraction::rtrn_abs() const
+	{
+		if (num < 0)
+		{
+			fraction temp(-num, den);
+			return temp;
+		}
+		else
+		{
+			fraction temp2(num, den);
+			return temp2;
+		}
+	}
+}
+	#endif
+
+
+//===========================================================================
+//I recommend using this library in C++ 11 or higher
+
+//logs:
+//8/20/2026, Added basic calculation
+//8/21/2026, Added int and double compatibility
+//8/22/2026，Added some stuff and fixed bugs
+//8/23/2026，Addded useful functions like pow_int
+//8/26/2026,Optimized the algorithm.
+
+// Verision: 1.0.0
+// Welcome to apply to Xiamen No.6 High School
+// If there are any problems with my code, feel free to email me at: 3594929067@qq.com
+// I will reply if im not in school
+//=============================================================================
+
